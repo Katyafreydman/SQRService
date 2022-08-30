18f1ff430f2ac17c5c5e5bcf62ade74127281458
Катя Фрейдман <katyafreydman@gmail.com>
2022-08-30 23:07:08 +0300

    Changes

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -10,8 +10,6 @@ public class SQRServiceTest {
     public void countSquares() {
 
         SQRService service = new SQRService();
-        //проверяем диапозон 200,300, тест проходит
-
         int actual = service.calculate(200, 300);
         int expected = 3;
         assertEquals(expected, actual);
@@ -20,7 +18,6 @@ public class SQRServiceTest {
     @Test
     public void countSquaresNearlimit() {
         SQRService service = new SQRService();
-        //проверяем диапозон 199,301, тест  проходит
         int actual = service.calculate(199, 301);
         int expected = 3;
         assertEquals(expected, actual);
@@ -29,7 +26,7 @@ public class SQRServiceTest {
     @Test
     public void countSquaresOverlimit() {
         SQRService service = new SQRService();
-        //проверяем диапозон 100,400, тест  проходит
+
         int actual = service.calculate(201, 301);
         int expected = 3;
         assertEquals(expected, actual);
